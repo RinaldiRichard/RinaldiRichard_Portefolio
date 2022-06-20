c4e6035582f2ab7ea1e5104a9d6cfcf6c268c83f
Rinaldi Richard <rinaldi.richatd@gmail.com>
2022-06-20 18:07:55 +0200

    ajout btn go top et son js

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
-const hamburgerToggler = document.querySelector(".hamburger")
+const hamburgerToggler = document.querySelector(".hamburger");
 const navLinksContainer = document.querySelector(".navlinks-container");
-
+console.log(window.scrollY);
 const toggleNav = () => {
-  hamburgerToggler.classList.toggle("open")
+  hamburgerToggler.classList.toggle("open");
 
-  const ariaToggle = hamburgerToggler.getAttribute("aria-expanded") === "true" ?  "false" : "true";
-  hamburgerToggler.setAttribute("aria-expanded", ariaToggle)
+  const ariaToggle =
+    hamburgerToggler.getAttribute("aria-expanded") === "true"
+      ? "false"
+      : "true";
+  hamburgerToggler.setAttribute("aria-expanded", ariaToggle);
 
-  navLinksContainer.classList.toggle("open")
-}
-hamburgerToggler.addEventListener("click", toggleNav)
+  navLinksContainer.classList.toggle("open");
+};
+hamburgerToggler.addEventListener("click", toggleNav);
 
-new ResizeObserver(entries => {
-  if(entries[0].contentRect.width <= 900){
-    navLinksContainer.style.transition = "transform 0.3s ease-out"
+new ResizeObserver((entries) => {
+  if (entries[0].contentRect.width <= 900) {
+    navLinksContainer.style.transition = "transform 0.3s ease-out";
   } else {
-    navLinksContainer.style.transition = "none"
+    navLinksContainer.style.transition = "none";
   }
-}).observe(document.body)
+}).observe(document.body);
 
+const btn = document.querySelector(".btnArrow");
+
+btn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+window.onscroll = () => {
+  if (window.scrollY > 400) {
+    document.querySelector(".btnArrow").style.opacity = "1";
+  } else {
+    document.querySelector(".btnArrow").style.opacity = "0";
+  }
+};
